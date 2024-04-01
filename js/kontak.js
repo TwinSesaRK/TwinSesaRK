4c0fbe7a90899f89c809fd1998dca3345924d9a8
TwinSesaRK <twinsesa4@gmail.com>
2024-04-02 01:52:25 +0700

    first commit

---
+++ js/kontak.js
@@ -0,0 +1,6 @@
+function submitData(){
+    var nama = document.getElementById("nama").value;
+    var pesan = document.getElementById("pesan").value;
+
+    alert( "Nama : " + nama + "\n Pesan : " + pesan);
+}
